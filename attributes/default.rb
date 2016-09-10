b22eafb4b60d7d9e18ffe7aa9e48826c6b60475f
Jose Manuel Tobar <ilovemysillybanana@gmail.com>
2016-09-10 15:06:22 -0500

    Added Bash and folder permissions to mabi user

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,8 @@
 default['mabi']['user'] = 'mabi'
+default['mabi']['group'] = 'mabi'
 default['mabi']['home'] = '/home/mabi'
 default['mabi']['shell'] = '/bin/bash'
+default['mabi']['password'] = '$1$znJ0xjBn$4Cj5XO93tWUWzW4XytC7M.'
 
 default['authorization']['sudo']['users'] << node['mabi']['user']
 default['authorization']['sudo']['users'] << 'vagrant'
--- recipes/general_setup.rb
+++ recipes/general_setup.rb
@@ -1,13 +1,27 @@
-directory "#{node['mabi']['home']}" do
-  recursive true
+group "#{node['mabi']['group']}" do
+  gid '1234'
+  action :create
 end
 
 user "#{node['mabi']['user']}" do
   comment 'A user for the server'
   home "#{node['mabi']['home']}"
+  group '1234'
   shell "#{node['mabi']['shell']}"
   password "#{node['mabi']['password']}"
   action :create
+end
+
+directory "#{node['mabi']['home']}" do
+  owner "#{node['mabi']['user']}"
+  group "#{node['mabi']['group']}"
+  recursive true
+end
+
+template "#{node['mabi']['home']}/.bashrc" do
+  source 'bashrc.erb'
+  owner "#{node['mabi']['user']}"
+  group "#{node['mabi']['group']}"
 end
 
 sudo "#{node['mabi']['user']}" do
